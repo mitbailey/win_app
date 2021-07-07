0f4e91331dcef9bd478b253dbef833c0fdfb9fe7
Mit Bailey <mitbailey99@gmail.com>
2021-07-07 12:03:14 -0400

    Added forgotten.

---
+++ include/test.hpp
@@ -0,0 +1,17 @@
+/**
+ * @file test.hpp
+ * @author Mit Bailey (mitbailey99@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021.07.07
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+/**
+ * @brief An example function to demonstrate the inclusion of .hpp and .cpp files.
+ * 
+ * @return int Always 1.
+ */
+int my_example_function(void);
---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file main.cpp
+ * @author Mit Bailey (mitbailey99@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021.07.07
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include <iostream>
+#include "test.hpp"
+
+int main (void)
+{
+    std::cout << "Hello, world!" << std::endl;
+
+    my_example_function();    
+
+    getchar();
+
+    return 1;
+}
